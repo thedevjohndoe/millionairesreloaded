7763d0b4b3410003e6aec1aa714543d500d8086f
Dev John <thedevjohndoe@gmail.com>
2020-10-18 17:52:55 +0200

    Site Name on Dashboard

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Global Goal Diggers</title>
+    <title><?php echo(SITE_NAME); ?></title>
     <link rel="stylesheet" href="assets/scss/index.css?v=<?php echo(time()); ?>">
     <script src="assets/js/index.js"></script>
 </head>
--- assets/templates/home.php
+++ assets/templates/home.php
@@ -18,32 +18,20 @@ get_header();
             </div>
             <div class="content__section">
                 <div class="section__header section__header--dashboard">
-                    <h1>Hello <?php echo($_SESSION['user_firstname']); ?>! Welcome to your Global Goal Diggers Dashboard.</h1>
+                    <h1>Hello <?php echo($_SESSION['user_firstname']); ?>! Welcome to your <?php echo(SITE_NAME); ?> Dashboard.</h1>
                     <p>Join us on whatsapp.<br/>
-                    <!-- Group 1: <a href="https://chat.whatsapp.com/Db2vFQHrWx1JhTPb6UAvDq">https://chat.whatsapp.com/Db2vFQHrWx1JhTPb6UAvDq</a><br/>
-                    Group 2: <a href="https://chat.whatsapp.com/DvkX0shrfZ76ydX2YYtNKA">https://chat.whatsapp.com/DvkX0shrfZ76ydX2YYtNKA</a><br/>
-                    Group 3: <a href="https://chat.whatsapp.com/Id0shyquXQs7y2GJJBtHgU">https://chat.whatsapp.com/Id0shyquXQs7y2GJJBtHgU</a><br/> -->
-                    Group 4: <a href="https://chat.whatsapp.com/LrhMwx1Zd156Pwmg1N9FPc">https://chat.whatsapp.com/LrhMwx1Zd156Pwmg1N9FPc</a></p>
+                    Click here: <a href="https://chat.whatsapp.com/LrhMwx1Zd156Pwmg1N9FPc">https://chat.whatsapp.com/LrhMwx1Zd156Pwmg1N9FPc</a></p>
                 </div>
                 <div class="section__content">
                     <div class="auction-times">
                         <div class="container">
                             <div class="container__column container__column--full">
-                                <h3>AUCTION TIMES: 06:00, 12:00 & 18:00 EVERY DAY!</h3>
+                                <h3>AUCTION TIME: 18:00, EVERY DAY</h3>
                             </div>
                         </div>
                     </div>
                     <div class="dashboard-container">
                         <div class="container">
-                            <div class="container__column container__column--quarter container__column--dashboard">
-                                <div class="column__header">
-                                    <label>Goal:</label>
-                                </div>
-                                <div class="column__content">
-                                    <!-- <label><?php echo($money_format->formatCurrency($target, "ZAR")); ?></label> -->
-                                    <label>On Hold</label>
-                                </div>
-                            </div>
                             <div class="container__column container__column--quarter container__column--dashboard">
                                 <div class="column__header">
                                     <label>Balance:</label>
@@ -66,6 +54,15 @@ get_header();
                                 </div>
                                 <div class="column__content">
                                     <label><?php echo($money_format->formatCurrency($info['interest'], "ZAR")); ?></label>
+                                </div>
+                            </div>
+                            <div class="container__column container__column--quarter container__column--dashboard">
+                                <div class="column__header">
+                                    <label>Bonus:</label>
+                                </div>
+                                <div class="column__content">
+                                    <!-- <label><?php echo($money_format->formatCurrency($target, "ZAR")); ?></label> -->
+                                    <label>R0,00</label>
                                 </div>
                             </div>
                         </div>
